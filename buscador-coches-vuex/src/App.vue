5b359d93a4184f6a8714da057fdf0718e37ef68e
jorge7447 <jorge.sandoval@correounivalle.edu.co>
2019-03-29 17:31:44 -0500

    118

---
+++ buscador-coches-vuex/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <vuex-search-form></vuex-search-form>
+    <vuex-search-list></vuex-search-list>
+  </div>
+</template>
+
+<script>
+  import VuexSearchList from './components/VuexSearchList'
+  import VuexSearchForm from './components/VuexSearchForm'
+  export default {
+    name: 'app',
+    components: {
+      VuexSearchList,
+      VuexSearchForm
+    }
+  }
+</script>
